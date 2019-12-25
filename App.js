57167c23e054d76803c1d9f6dcda13336107194c
Yeonho Jang <siabard@gmail.com>
2019-12-25 23:10:48 +0900

    initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+    <Text>Open up App.js to start with work. Functional</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+export default App;
